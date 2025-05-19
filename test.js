98dd69252006c0123ee9819130d112fa1fcfa970
Somajit Dey <73181168+SomajitDey@users.noreply.github.com>
2025-05-19 16:46:39 +0530

    2.0.0

--- test.js
+++ test.js
@@ -1,15 +1,55 @@
 import { mimeToExtensions, extensionToMime } from './index.js';
+import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
-const mimeType = 'application/javascript';
-const extension = 'js';
+describe('Testing index.js', () => {
+  test('mimeToExtensions()', async () => {
+    const extension = 'js';
+    const mimeTypeSpecs = [
+      'application/javascript; charset=utf-8',
+      'application/javascript'
+    ];
 
-assert.equal(
-  await mimeToExtensions(mimeType)
-    .then((extensions) => extensions.includes(extension)),
-  true);
+    for (const mimeTypeSpec of mimeTypeSpecs) {
+      assert.equal(
+        await mimeToExtensions(mimeTypeSpec)
+          .then((extensions) => extensions.includes(extension)),
+        true
+      );
+    }
+  });
 
-assert.equal(
-  await extensionToMime(extension)
-    .then((mimeTypes) => mimeTypes.includes(mimeType)),
-  true);
+  test('Error for mimeToExtensions()', async () => {
+    const fakeMimeType = 'type/subtype';
+    assert.rejects(
+      mimeToExtensions(fakeMimeType),
+      { message: 'Not Found' }
+    );
+  });
+
+  test('extensionToMime()', async () => {
+    const mimeType = 'application/javascript';
+    const args = [
+      'dir/subdir/path.js',
+      'js',
+      '.js',
+      'path.js',
+      'dir/subdir/path.version.js'
+    ];
+    for (const arg of args) {
+      assert.equal(
+        await extensionToMime(arg)
+          .then((mimeTypes) => mimeTypes.includes(mimeType)),
+        true
+      );
+    }
+  });
+
+  test('Error for extensionToMime()', async () => {
+    const fakeExtension = 'extension';
+    assert.rejects(
+      extensionToMime(fakeExtension),
+      { message: 'Not Found' }
+    );
+  });
+});
